c748c3c19a4575599043193d9c3a54fd5377f1fc
bennetp <50826643+bennetp@users.noreply.github.com>
2019-05-24 19:05:31 +0200

    One small step for progress, one huge for hedacheadsda

--- Final project/final_project_init.m
+++ Final project/final_project_init.m
@@ -18,7 +18,7 @@ memSize = 1024; % Size of memory holding bitstream.
 stepSize = 1/sampleRate;
 stopTime = 10e3*stepSize;
 
-detection_threshold = 100; %% Hardcoded???
+detection_threshold = 5e5; %% Hardcoded???
 pilot_bits = [0 0 0 0 0 0 0 0 0 1 1 0 0 0 0]';
 pilot_size = length(pilot_bits);
 memory_data = [pilot_bits; randsrc(memSize-pilot_size,1,[0 1])]; % BPSK
@@ -27,11 +27,13 @@ fir.coeffs = fi(rcosdesign(0.8,5,3*sampleRate/dataRate,'sqrt'), 1, wordLength-2)
 barker = [1 1 1 -1 1];
 barker_fir = fi(conv(rcosdesign(0.8,5,3*sampleRate/dataRate,'sqrt'),upsample(barker,3*sampleRate/dataRate)));
 
-moving_avg_fir = fi(ones(1,50)/50);
+moving_avg_fir = fi(ones(1,50)/50,1,8);
 
 mapper_LUT_I = [1 0.70710678 0.70710678 0 -0.70710678 0 -1 -0.70710678];
 mapper_LUT_Q = [0 0.70710678 -0.70710678 -1 0.70710678 1 0 -0.70710678];
 
+LUT_rx_map = [6 7 3 2 0 1 5 4 6];
+
 carrierFreq = 5e6;
 
-channel_delay = 117;
+channel_delay = 119;
